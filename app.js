c6778d4c0c6725360571f0d3eec57c0d10bda04a
Brandon Arroyo <brandon.jarroyo@outlook.com>
2017-07-24 18:48:42 -0700

    a giant mess

--- app.js
+++ app.js
@@ -1,32 +1,22 @@
 'use strict';
+var holdImages = [];
+var allImages = ['one.jpg', 'two.jpg', 'three.jpg'];
 
-
-
-// function Image(number) {
-//   this.name = number;
-//   this.source = 'img/' + this.name + '.jpg';
-//   this.timesShown = 0;
-//   Image.all.push(this);
-// }
-
-Image.all = [];
-Image.allNames = ['one.jpg','two.jpg', 'three.jpg', 'four.jpg', 'five.jpg', 'six.jpg', 'seven.jpg', 'eight.jpg', 'nine.jpg', 'ten.jpg', 'eleven.jpg', 'twelve.jpg', 'thirteen.jpg', 'fourteen.jpg', 'fifteen.jpg', 'sixteen.jpg', 'seventeen.jpg', 'eighteen.jpg', 'nineteen.jpg', 'twenty.jpg'];
-
-for(var i = 0; i < Image.allNames.length; i++){
-  new Image(Image.allNames[i]);
+function image(name, source, timesClicked, timesShown){
+  this.name = name;
+  this.source = source;
+  this.timesClicked = 0;
+  this.timesShown = 0;
+  var random = Math.floor(Math.random() * allImages[i]);
+  random.push(holdImages);
 }
-//
-// Image.imgEl = document.getElementById('the_image');
-//
+
+
+
+
+document.getElementById('id').addEventListener('click', image)
+
 function randomImage(){
-  console.log('shit');
-  var randomIndex = Math.floor(Math.random() * Image.all.length);
-  Image.imgEl.src = Image.all[randomIndex].source;
-  Image.imgEl.alt = Image.all[randomIndex].name;
-  Image.all[randomIndex].timesShown += 1;
-  // console.log(Image.all[randomIndex].name + ' has been shown ' + Image.all[randomIndex].timesShown + ' times');
+  var random = Math.floor(Math.random() * allImages[i]);
 }
-document.getElementById('the_image').addEventListener('click',randomImage);
-//
-// randomImage();
-image();
+]
